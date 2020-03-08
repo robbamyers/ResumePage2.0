ad715a522e8a4b5a5434ff03f7e40172914b9c1e
Robbie Myers <robbamyers@icloud.com>
2020-03-08 01:53:07 -0500

    anchor tags, sticky navbar, started bootstrap grid

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,3 @@
-
 //BUTTONS
     //Collapsible
 var coll = document.getElementsByClassName("collapsible");
@@ -14,5 +13,24 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+}
+
+//HEADER
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
 
